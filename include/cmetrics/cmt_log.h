51bf674f506164804c1ad9e66120da0b18ea9ce4
Phillip Whelan <phil@calyptia.com>
2021-07-21 18:32:58 -0400

    log: fix __FILENAME__ undeclared.
    Signed-off-by: Phillip Whelan <phil@calyptia.com>

--- include/cmetrics/cmt_log.h
+++ include/cmetrics/cmt_log.h
@@ -38,27 +38,27 @@ void cmt_log_print(void *ctx, int level, const char *file, int line,
 int cmt_errno_print(int errnum, const char *file, int line);
 
 #define cmt_log_error(ctx, fmt, ...)                    \
-    cmt_log_print(ctx, CMT_LOG_ERROR, __FILENAME__,     \
+    cmt_log_print(ctx, CMT_LOG_ERROR, __FILE__,     \
                   __LINE__, fmt, ##__VA_ARGS__)
 
 #define cmt_log_warn(ctx, fmt, ...)                 \
-    cmt_log_print(ctx, CMT_LOG_WARN, __FILENAME__,  \
+    cmt_log_print(ctx, CMT_LOG_WARN, __FILE__,  \
                   __LINE__, fmt, ##__VA_ARGS__)
 
 #define cmt_log_info(ctx, fmt, ...)                 \
-    cmt_log_print(ctx, CMT_LOG_INFO, __FILENAME__,  \
+    cmt_log_print(ctx, CMT_LOG_INFO, __FILE__,  \
                   __LINE__, fmt, ##__VA_ARGS__)
 
 #define cmt_log_debug(ctx, fmt, ...)                \
-    cmt_log_print(ctx, CMT_LOG_DEBUG, __FILENAME__, \
+    cmt_log_print(ctx, CMT_LOG_DEBUG, __FILE__, \
                   __LINE__, fmt, ##__VA_ARGS__)
 
 #define cmt_log_trace(ctx, fmt, ...)                \
-    cmt_log_print(ctx, CMT_LOG_TRACE, __FILENAME__, \
+    cmt_log_print(ctx, CMT_LOG_TRACE, __FILE__, \
                   __LINE__, fmt, ##__VA_ARGS__)
 
-#ifdef __FILENAME__
-#define cmt_errno() cmt_errno_print(errno, __FILENAME__, __LINE__)
+#ifdef __FILE__
+#define cmt_errno() cmt_errno_print(errno, __FILE__, __LINE__)
 #else
 #define cmt_errno() cmt_errno_print(errno, __FILE__, __LINE__)
 #endif
