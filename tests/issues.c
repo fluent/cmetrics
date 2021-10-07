1b14722c5f49f2f2271624afee73f25fa938d3f5
Leonardo Alminana <leonardo@calyptia.com>
2021-10-07 14:03:53 -0300

    tests: fixed cleanup and code style
    Signed-off-by: Leonardo Alminana <leonardo@calyptia.com>

--- tests/issues.c
+++ tests/issues.c
@@ -50,17 +50,15 @@ static struct cmt *generate_encoder_test_data()
 
 void test_issue_54()
 {
-    int ret;
-    size_t offset = 0;
-    char *mp1_buf = NULL;
-    size_t mp1_size = 1;
-    char *mp2_buf = NULL;
-    size_t mp2_size = 2;
-    struct cmt *cmt1 = NULL;
-    struct cmt *cmt2 = NULL;
-    cmt_sds_t text_result;
-    const char expected_text[] = "1970-01-01T00:00:00.000000000Z kubernetes_load{tag1=\"tag1\",tag2=\"tag2\"} = 10\n" \
-                                 "1970-01-01T00:00:00.000000000Z kubernetes_cpu{tag1=\"tag1\",tag2=\"tag2\"} = 10\n";
+    const char  expected_text[] = "1970-01-01T00:00:00.000000000Z kubernetes_load{tag1=\"tag1\",tag2=\"tag2\"} = 10\n" \
+                                  "1970-01-01T00:00:00.000000000Z kubernetes_cpu{tag1=\"tag1\",tag2=\"tag2\"} = 10\n";
+    cmt_sds_t   text_result;
+    size_t      mp1_size;
+    char       *mp1_buf;
+    size_t      offset;
+    int         result;
+    struct cmt *cmt2;
+    struct cmt *cmt1;
 
     cmt_initialize();
 
@@ -73,24 +71,23 @@ void test_issue_54()
     cmt_label_add(cmt1, "tag2", "tag2");
 
     /* CMT1 -> Msgpack */
-    ret = cmt_encode_msgpack_create(cmt1, &mp1_buf, &mp1_size);
-    TEST_CHECK(0 == ret);
+    result = cmt_encode_msgpack_create(cmt1, &mp1_buf, &mp1_size);
+    TEST_CHECK(0 == result);
 
     /* Msgpack -> CMT2 */
-    ret = cmt_decode_msgpack_create(&cmt2, mp1_buf, mp1_size, &offset);
-    TEST_CHECK(0 == ret);
+    offset = 0;
+    result = cmt_decode_msgpack_create(&cmt2, mp1_buf, mp1_size, &offset);
+    TEST_CHECK(0 == result);
 
     text_result = cmt_encode_text_create(cmt2);
+
     TEST_CHECK(NULL != text_result);
     TEST_CHECK(0 == strcmp(text_result, expected_text));
 
-    cmt_destroy(cmt1);
     cmt_encode_text_destroy(text_result);
     cmt_decode_msgpack_destroy(cmt2);
     cmt_encode_msgpack_destroy(mp1_buf);
-    cmt_encode_msgpack_destroy(mp2_buf);
-
-    // TEST_CHECK(global_counter == EXPECTED_VALUE);
+    cmt_destroy(cmt1);
 }
 
 TEST_LIST = {
