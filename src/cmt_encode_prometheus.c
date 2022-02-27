9eb1cac1aeabdc2e9c2a5f9dd0d12665d9791e6d
Eduardo Silva <eduardo@calyptia.com>
2022-02-26 23:06:42 -0600

    encode: prometheus: add support for Histograms
    Signed-off-by: Eduardo Silva <eduardo@calyptia.com>

--- src/cmt_encode_prometheus.c
+++ src/cmt_encode_prometheus.c
@@ -25,6 +25,8 @@
 #include <cmetrics/cmt_sds.h>
 #include <cmetrics/cmt_counter.h>
 #include <cmetrics/cmt_gauge.h>
+#include <cmetrics/cmt_histogram.h>
+
 #include <cmetrics/cmt_untyped.h>
 #include <cmetrics/cmt_compat.h>
 
@@ -87,21 +89,35 @@ static void metric_banner(cmt_sds_t *buf, struct cmt_map *map,
     else if (map->type == CMT_GAUGE) {
         cmt_sds_cat_safe(buf, " gauge\n", 7);
     }
+    else if (map->type == CMT_HISTOGRAM) {
+        cmt_sds_cat_safe(buf, " histogram\n", 11);
+    }
     else if (map->type == CMT_UNTYPED) {
         cmt_sds_cat_safe(buf, " untyped\n", 9);
     }
 }
 
 static void append_metric_value(cmt_sds_t *buf, struct cmt_metric *metric,
-                                int add_timestamp)
+                                int bucket_id, int add_timestamp)
 {
     int len;
     double val;
     uint64_t ts;
     char tmp[128];
 
-    /* Retrieve metric value */
-    val = cmt_metric_get_value(metric);
+    /*
+     * Retrieve metric value
+     * ---------------------
+     * if 'bucket_id' == -1, means we need to get the direct value associated
+     * to the metric, otherwise look for the value from hist_buckets[bucket_id],
+     * typically used for histograms.
+     */
+    if (bucket_id < 0) {
+        val = cmt_metric_get_value(metric);
+    }
+    else {
+        val = cmt_metric_hist_bucket_get_value(metric, bucket_id);
+    }
 
     if (add_timestamp) {
         ts = cmt_metric_get_timestamp(metric);
@@ -117,14 +133,61 @@ static void append_metric_value(cmt_sds_t *buf, struct cmt_metric *metric,
     cmt_sds_cat_safe(buf, tmp, len);
 }
 
+static int add_label(cmt_sds_t *buf, cmt_sds_t key, cmt_sds_t val)
+{
+    cmt_sds_cat_safe(buf, key, cmt_sds_len(key));
+    cmt_sds_cat_safe(buf, "=\"", 2);
+    metric_escape(buf, val, true);
+    cmt_sds_cat_safe(buf, "\"", 1);
+
+    return 1;
+}
+
+static int add_static_labels(struct cmt *cmt, cmt_sds_t *buf)
+{
+    int count = 0;
+    int total = 0;
+    struct mk_list *head;
+    struct cmt_label *label;
+
+    total = mk_list_size(&cmt->static_labels->list);
+    mk_list_foreach(head, &cmt->static_labels->list) {
+        label = mk_list_entry(head, struct cmt_label, _head);
+
+        count += add_label(buf, label->key, label->val);
+        if (count < total) {
+            cmt_sds_cat_safe(buf, ",", 1);
+        }
+    }
+
+    return count;
+}
+
+struct prom_fmt {
+    int metric_name;   /* metric name already set ? */
+    int brace_open;    /* first brace open ? */
+    int labels_count;  /* number of labels aready added */
+    int bucket_id;
+};
+
+static void format_init(struct prom_fmt *fmt)
+{
+    fmt->metric_name = CMT_FALSE;
+    fmt->brace_open = CMT_FALSE;
+    fmt->labels_count = 0;
+    fmt->bucket_id = -1;
+}
+
 static void format_metric(struct cmt *cmt,
                           cmt_sds_t *buf, struct cmt_map *map,
-                          struct cmt_metric *metric, int add_timestamp)
+                          struct cmt_metric *metric, int add_timestamp,
+                          struct prom_fmt *fmt)
 {
     int i;
-    int n;
     int count = 0;
     int static_labels = 0;
+    int defined_labels = 0;
+    int total_labels = 0;
     struct cmt_map_label *label_k;
     struct cmt_map_label *label_v;
     struct mk_list *head;
@@ -134,68 +197,117 @@ static void format_metric(struct cmt *cmt,
     opts = map->opts;
 
     /* Metric info */
-    cmt_sds_cat_safe(buf, opts->fqname, cmt_sds_len(opts->fqname));
+    if (!fmt->metric_name) {
+        cmt_sds_cat_safe(buf, opts->fqname, cmt_sds_len(opts->fqname));
+    }
 
     /* Static labels */
     static_labels = cmt_labels_count(cmt->static_labels);
-    if (static_labels > 0) {
+    defined_labels = mk_list_size(&metric->labels);
+
+    if (!fmt->brace_open && (static_labels + defined_labels > 0)) {
         cmt_sds_cat_safe(buf, "{", 1);
-
-        mk_list_foreach(head, &cmt->static_labels->list) {
-            count++;
-            slabel = mk_list_entry(head, struct cmt_label, _head);
-            cmt_sds_cat_safe(buf, slabel->key, cmt_sds_len(slabel->key));
-            cmt_sds_cat_safe(buf, "=\"", 2);
-            metric_escape(buf, slabel->val, true);
-            cmt_sds_cat_safe(buf, "\"", 1);
-
-            if (count < static_labels) {
-                cmt_sds_cat_safe(buf, ",", 1);
-            }
-        }
     }
 
-    /* Append static labels */
-    n = mk_list_size(&metric->labels);
-    if (n > 0) {
-        if (static_labels > 0) {
+    if (static_labels > 0) {
+        /* if some labels were added before, add the separator */
+        if (fmt->labels_count > 0) {
             cmt_sds_cat_safe(buf, ",", 1);
         }
-        else {
-            cmt_sds_cat_safe(buf, "{", 1);
+        fmt->labels_count += add_static_labels(cmt, buf);
+    }
+
+    /* Append api defined labels */
+    if (defined_labels > 0) {
+        if (fmt->labels_count > 0) {
+            cmt_sds_cat_safe(buf, ",", 1);
         }
 
-        label_k = mk_list_entry_first(&map->label_keys, struct cmt_map_label, _head);
-
         i = 1;
+        label_k = mk_list_entry_first(&map->label_keys, struct cmt_map_label, _head);
         mk_list_foreach(head, &metric->labels) {
             label_v = mk_list_entry(head, struct cmt_map_label, _head);
 
-            cmt_sds_cat_safe(buf, label_k->name, cmt_sds_len(label_k->name));
-            cmt_sds_cat_safe(buf, "=\"", 2);
-            metric_escape(buf, label_v->name, true);
+            fmt->labels_count += add_label(buf, label_k->name, label_v->name);
 
-            if (i < n) {
-                cmt_sds_cat_safe(buf, "\",", 2);
-            }
-            else {
-                cmt_sds_cat_safe(buf, "\"", 1);
+            if (i < defined_labels) {
+                cmt_sds_cat_safe(buf, ",", 1);
             }
             i++;
-
             label_k = mk_list_entry_next(&label_k->_head, struct cmt_map_label,
                                          _head, &map->label_keys);
         }
-        cmt_sds_cat_safe(buf, "}", 1);
-        append_metric_value(buf, metric, add_timestamp);
-    }
-    else {
-        if (static_labels > 0) {
-            cmt_sds_cat_safe(buf, "}", 1);
-        }
-        append_metric_value(buf, metric, add_timestamp);
     }
 
+    if (fmt->labels_count > 0) {
+        cmt_sds_cat_safe(buf, "}", 1);
+    }
+
+    append_metric_value(buf, metric, fmt->bucket_id, add_timestamp);
+}
+
+static cmt_sds_t bucket_value_to_string(double val)
+{
+    int len;
+    cmt_sds_t str;
+
+    str = cmt_sds_create_size(64);
+    if (!str) {
+        return NULL;
+    }
+
+    len = snprintf(str, 64, "%g", val);
+    cmt_sds_len_set(str, len);
+
+    if (!strchr(str, '.')) {
+        cmt_sds_cat_safe(&str, ".0", 2);
+    }
+
+    return str;
+}
+
+static void format_bucket(struct cmt *cmt,
+                          cmt_sds_t *buf, struct cmt_map *map,
+                          struct cmt_metric *metric, int add_timestamp)
+{
+    int i;
+    cmt_sds_t val;
+    struct cmt_histogram *histogram;
+    struct cmt_histogram_buckets *bucket;
+    struct cmt_opts *opts;
+    struct prom_fmt fmt = {0};
+
+    histogram = (struct cmt_histogram *) map->parent;
+    bucket = histogram->buckets;
+    opts = map->opts;
+
+    for (i = 0; i <= bucket->count; i++) {
+        /* metric name */
+        cmt_sds_cat_safe(buf, opts->fqname, cmt_sds_len(opts->fqname));
+        cmt_sds_cat_safe(buf, "_bucket", 7);
+
+        /* upper bound */
+        cmt_sds_cat_safe(buf, "{le=\"", 5);
+
+        if (i < bucket->count) {
+            val = bucket_value_to_string(bucket->upper_bounds[i]);
+            cmt_sds_cat_safe(buf, val, cmt_sds_len(val));
+            cmt_sds_destroy(val);
+        }
+        else {
+            cmt_sds_cat_safe(buf, "+Inf", 4);
+        }
+        cmt_sds_cat_safe(buf, "\"", 1);
+
+        /* configure formatter */
+        fmt.metric_name  = CMT_TRUE;
+        fmt.brace_open   = CMT_TRUE;
+        fmt.labels_count = 1;
+        fmt.bucket_id    = i;
+
+        /* append metric labels, value and timestamp */
+        format_metric(cmt, buf, map, metric, add_timestamp, &fmt);
+    }
 }
 
 static void format_metrics(struct cmt *cmt, cmt_sds_t *buf, struct cmt_map *map,
@@ -204,12 +316,21 @@ static void format_metrics(struct cmt *cmt, cmt_sds_t *buf, struct cmt_map *map,
     int banner_set = CMT_FALSE;
     struct mk_list *head;
     struct cmt_metric *metric;
+    struct prom_fmt fmt = {0};
 
     /* Simple metric, no labels */
     if (map->metric_static_set) {
         metric_banner(buf, map, &map->metric);
         banner_set = CMT_TRUE;
-        format_metric(cmt, buf, map, &map->metric, add_timestamp);
+
+        if (map->type == CMT_HISTOGRAM) {
+            /* Histogram needs to format the buckets, one line per bucket */
+            format_bucket(cmt, buf, map, &map->metric, add_timestamp);
+        }
+        else {
+            format_init(&fmt);
+            format_metric(cmt, buf, map, &map->metric, add_timestamp, &fmt);
+        }
     }
 
     if (mk_list_size(&map->metrics) > 0) {
@@ -221,7 +342,16 @@ static void format_metrics(struct cmt *cmt, cmt_sds_t *buf, struct cmt_map *map,
 
     mk_list_foreach(head, &map->metrics) {
         metric = mk_list_entry(head, struct cmt_metric, _head);
-        format_metric(cmt, buf, map, metric, add_timestamp);
+
+        /* Format the metric based on its type */
+        if (map->type == CMT_HISTOGRAM) {
+            /* Histogram needs to format the buckets, one line per bucket */
+            format_bucket(cmt, buf, map, metric, add_timestamp);
+        }
+        else {
+            format_init(&fmt);
+            format_metric(cmt, buf, map, metric, add_timestamp, &fmt);
+        }
     }
 }
 
@@ -232,6 +362,7 @@ cmt_sds_t cmt_encode_prometheus_create(struct cmt *cmt, int add_timestamp)
     struct mk_list *head;
     struct cmt_counter *counter;
     struct cmt_gauge *gauge;
+    struct cmt_histogram *histogram;
     struct cmt_untyped *untyped;
 
     /* Allocate a 1KB of buffer */
@@ -250,6 +381,12 @@ cmt_sds_t cmt_encode_prometheus_create(struct cmt *cmt, int add_timestamp)
     mk_list_foreach(head, &cmt->gauges) {
         gauge = mk_list_entry(head, struct cmt_gauge, _head);
         format_metrics(cmt, &buf, gauge->map, add_timestamp);
+    }
+
+    /* Histograms */
+    mk_list_foreach(head, &cmt->histograms) {
+        histogram = mk_list_entry(head, struct cmt_histogram, _head);
+        format_metrics(cmt, &buf, histogram->map, add_timestamp);
     }
 
     /* Untyped */
