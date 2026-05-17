17901467ca1cf0f10fcfbac4a9b732c62872e6c2
Phillip Whelan <pwhelan@exis.cl>
2026-05-17 17:00:07 -0400

    cmt_map_metrics_expire: expire metrics older than expiration timestamp, return void.
    Signed-off-by: Phillip Whelan <pwhelan@exis.cl>

--- src/cmt_map.c
+++ src/cmt_map.c
@@ -336,7 +336,7 @@ void destroy_label_list(struct cfl_list *label_list)
 
 /* This function can be used to expire untouched metrics.
  */
-int cmt_map_metrics_expire(struct cmt_map *map, uint64_t expiration)
+void cmt_map_metrics_expire(struct cmt_map *map, uint64_t expiration)
 {
     struct cfl_list *tmp;
     struct cfl_list *head;
@@ -344,9 +344,8 @@ int cmt_map_metrics_expire(struct cmt_map *map, uint64_t expiration)
 
     cfl_list_foreach_safe(head, tmp, &map->metrics) {
         metric = cfl_list_entry(head, struct cmt_metric, _head);
-        if (metric->timestamp <= expiration) {
+        if (metric->timestamp < expiration) {
             cmt_map_metric_destroy(metric);
         }
     }
-    return 0;
 }
