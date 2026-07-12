7fb734598c6a1c0666abb8f815c89c390646976f
Eduardo Silva <eduardo@chronosphere.io>
2026-07-12 13:08:39 -0600

    opentelemetry: reduce metrics encoding allocation churn
    Signed-off-by: Eduardo Silva <eduardo@chronosphere.io>

--- benchmarks/run-perf.sh
+++ benchmarks/run-perf.sh
@@ -21,8 +21,15 @@ run_repeated lookup 5000 100000
 run_repeated update 5000 100000
 run_repeated update 1 5000000
 run_repeated prometheus 5000 100
+run_repeated opentelemetry 5000 100
+run_repeated opentelemetry-mixed 2000 100
 
 perf stat \
     -e cycles,instructions,branches,branch-misses,cache-misses \
     -r "$repetitions" \
     "$benchmark" lookup 5000 100000
+
+perf stat \
+    -e task-clock,cycles,instructions,branches,branch-misses,cache-misses \
+    -r "$repetitions" \
+    "$benchmark" opentelemetry 5000 500
